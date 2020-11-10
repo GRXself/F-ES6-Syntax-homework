7f642b8f83118141c3c4ef492ca5c5256a0cce04
Ruixing Gao <ruixing.gao@thoughtworks.com>
2020-11-10 22:18:22 +0800

    Complete two tasks.

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,18 @@
-const parseData
+const parseData = ({data, column}) => {
+    let output = [];
+    for (const user of data) {
+        output.push(constructUser(user, column));
+    }
+    console.log(output);
+    return output;
+};
+
+const constructUser = (userData, column) => {
+    let user = {};
+    column.forEach((property, index) => {
+        user[property.name] = userData[index];
+    });
+    return user;
+}
+
 export { parseData };
